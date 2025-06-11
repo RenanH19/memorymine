486adf2c89e35d4dc85381505d9ac68cfdb89d1b
Renan <renan.herculano@pucpr.edu.br>
2025-06-11 20:30:24 -0300

    sound teste2

--- src/components/GameCanvas.jsx
+++ src/components/GameCanvas.jsx
@@ -2,27 +2,33 @@ import React, { useState } from 'react';
 import Sketch from 'react-p5';
 import Player from '../p5/Player';
 import 'p5/lib/addons/p5.sound';
-import MusicManager from '../p5/audio/MusicManager';
+
 
 function GameCanvas() {
   
   let player;
   let music;
   
-
   const preload = (p5) => {
-    music = p5.loadSound('/assets/music/moongate.mp3')
+    music = p5.loadSound(('/assets/music/moongate.mp3'),() =>{
+      console.log('som carregou')
+    })
   }
 
   const setup = (p5, parentRef) => {
     p5.createCanvas(960, 640).parent(parentRef);
     player = new Player(p5, 100, 100, 800);
-    music.play();
+    
+    
   }
 
   const draw = (p5) => {
     p5.background(200);
     
+    if(p5.mouseIsPressed){
+      music.play();
+    }
+
     player.display();
     player.update();
 
---
+++ src/p5/levels/levelManager.js
@@ -0,0 +1,8 @@
+class levelManager{
+    constructor(map){
+        this.map = map
+        this.player = player
+        this.enemy = enemy
+        this.item = item
+    }
+}
